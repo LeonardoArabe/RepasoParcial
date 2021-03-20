cc74c8fe75831772587b711605fe73c05a9ce072
Leonardo Arabe Castedo <a2020122243@estudiantes.upsa.edu.bo>
2021-03-19 23:14:08 -0400

    Modelo incorporado

---
+++ RepasoParcialconDaniela/RepasoconDaniela/Models/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RepasoconDaniela.Models
+{
+    public class Country
+    {
+        [Key]
+        public string Name { get; set; }
+        [Required]
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public double Latlng { get; set; }
+        public string Timezones { get; set; }
+        public Currency Currencies { get; set; }
+        public string Flag { get; set; }
+    }
+}
